d9693c26ceeb33aa793c9c8c803cdaea8bbc4270
William Phan <william.da.phan@gmail.com>
2014-11-15 22:31:14 +0100

    Exercise 3 added

--- exercise 3/src/buffer/Buffer.java
+++ exercise 3/src/buffer/Buffer.java
@@ -3,11 +3,11 @@ package buffer;
 import se.lth.cs.realtime.RTError;
 
 class Buffer {
-	int available;		// Number of lines that are available. 
-	final int size=8;		// The max number of buffered lines.
-	String[] buffData;	// The actual buffer.
-	int nextToPut;		// Writers index.
-	int nextToGet;		// Readers index.
+	int available; // Number of lines that are available.
+	final int size = 8; // The max number of buffered lines.
+	String[] buffData; // The actual buffer.
+	int nextToPut; // Writers index.
+	int nextToGet; // Readers index.
 
 	Buffer() {
 		buffData = new String[size];
@@ -15,18 +15,31 @@ class Buffer {
 
 	synchronized void putLine(String inp) {
 		try {
-			while (available==size) wait();
+			while (available == size) wait();
 		} catch (InterruptedException exc) {
-			throw new RTError("Buffer.putLine interrupted: "+exc);
+			throw new RTError("Buffer.putLine interrupted: " + exc);
 		};
 		buffData[nextToPut] = new String(inp);
-		if (++nextToPut >= size) nextToPut = 0;
+		if (++nextToPut >= size)
+			nextToPut = 0;
 		available++;
 		notifyAll(); // Only notify() could wake up another producer.
 	}
 
 	synchronized String getLine() {
 		// Write the code that implements this method ...
-		return null;
+		try {
+			while (available == 0) wait();		//Väntar ifall det inte finns några available
+		} catch (InterruptedException exc) {
+			throw new RTError("Buffer.getLine interrupted: " + exc);
+		};	// Behövs ";"?
+		String out = buffData[nextToGet];
+		buffData[nextToGet] = null;
+		nextToGet++;
+		if(nextToGet >= size)		// Börja om på 0 om den överstiger 8
+			nextToGet = 0;
+		available--;
+		notifyAll();
+		return out;
 	}
 }
--- exercise 3/src/queue/YourMonitor.java
+++ exercise 3/src/queue/YourMonitor.java
@@ -2,36 +2,60 @@ package queue;
 
 class YourMonitor {
 	private int nCounters;
+	private int freeClerks;
+	private int lastCustomer;
+	private int servedCustomer;
+	private int currentID;
+	private boolean freeCounters[];
+
 	// Put your attributes here...
 
-	YourMonitor(int n) { 
+	YourMonitor(int n) {
 		nCounters = n;
+		freeCounters = new boolean[n]; // n st lediga counters
+	//	nextClerk = 0; // Börjar på 0 eller 99 efter klock
+
 		// Initialize your attributes here...
 	}
 
 	/**
-	 * Return the next queue number in the intervall 0...99. 
-	 * There is never more than 100 customers waiting.
+	 * Return the next queue number in the intervall 0...99. There is never more
+	 * than 100 nextCustomer waiting.
 	 */
-	synchronized int customerArrived() { 
+	synchronized int customerArrived() {
 		// Implement this method...
-		return 0;
+		lastCustomer++;
+		lastCustomer = lastCustomer % 100;	//Max 100
+		notifyAll(); // Här??
+		return lastCustomer;
 	}
 
 	/**
-	 * Register the clerk at counter id as free. Send a customer if any. 
+	 * Register the clerk at counter id as free. Send a customer if any.
 	 */
-	synchronized void clerkFree(int id) { 
+	synchronized void clerkFree(int id) {
 		// Implement this method...
+		if (!freeCounters[id]) { // Ifall det sker många knapptryckningar
+			freeCounters[id] = true;
+			freeClerks++;
+			notifyAll();
+		}
 	}
 
 	/**
-	 * Wait for there to be a free clerk and a waiting customer, then
-	 * return the cueue number of next customer to serve and the counter 
-	 * number of the engaged clerk.
+	 * Wait for there to be a free clerk and a waiting customer, then return the
+	 * cueue number of next customer to serve and the counter number of the
+	 * engaged clerk.
 	 */
-	synchronized DispData getDisplayData() throws InterruptedException { 
-		// Implement this method...
-		return null;
+	synchronized DispData getDisplayData() throws InterruptedException {
+		// Implement this method...	
+		while(lastCustomer == servedCustomer || freeClerks == 0) wait();
+		while(!freeCounters[currentID]) currentID = (currentID+1)%nCounters;
+		DispData thisCounter  = new DispData();
+		thisCounter.counter = currentID;
+		freeCounters[currentID] = false;
+		freeClerks--;
+		thisCounter.ticket = lastCustomer = (lastCustomer+1)%100;
+		return thisCounter;
 	}
 }
